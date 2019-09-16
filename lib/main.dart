cb7848eafccd59f21ae7fd55293beb9f19a41992
shweta <shweta.poonia@gohousr.com>
2019-09-16 22:13:59 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -43,9 +43,12 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
       Center(
         
-        child: Column(
-          children: <Widget>[quickAction(), serviceUpdate(), myLayoutWidget(), partnerOffers()],
-        ),
+
+  child : new SingleChildScrollView(
+         child: Column(
+              children: <Widget>[myLayoutWidget(),quickAction(), serviceUpdate(), partnerOffers(),myLayoutWidgetSecond()],
+         ),
+       ),
       ),
               ]
       )
@@ -363,6 +366,80 @@ Widget myLayoutWidget() {
               ),
               child: Text(
                 'Pay now to avoid late payment fees!',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+Widget myLayoutWidgetSecond() {
+  // wrap everything in a purple container
+  return Container(
+    margin: EdgeInsets.all(16.0),
+    padding: EdgeInsets.all(16.0),
+    decoration: BoxDecoration(
+        gradient: LinearGradient(
+          // Where the linear gradient begins and ends
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          // Add one stop for each color. Stops should increase from 0 to 1
+          stops: [0.1, 0.5, 0.7, 0.9],
+          colors: [
+            // Colors are easy thanks to Flutter's Colors class.
+            Colors.purple[800],
+            Colors.purple[700],
+            Colors.purple[600],
+            Colors.purple[400],
+          ],
+        ),
+        border: Border.all(),
+        borderRadius: BorderRadius.circular(10.0)),
+
+    // column of three rows
+    child: Column(
+      // this makes the column height hug its content
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        // first row
+        Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Icon(
+                Icons.hotel,
+                color: Colors.green,
+              ),
+            ),
+            Text(
+              'Upcoming Payment',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10.0, top: 50.0),
+              child: Icon(
+                Icons.arrow_forward,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+
+        // second row (single item)
+        Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 16.0,
+                horizontal: 10,
+              ),
+              child: Text(
+                'Rent for september . Pay now',
                 style: TextStyle(
                   color: Colors.white,
                 ),
